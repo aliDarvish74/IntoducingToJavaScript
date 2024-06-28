503733469e8a6cb44c9d72ee121f4f6c91c37688
aliDarvish74 <ali.darvish.sf@gmail.com>
2024-06-28 19:06:20 +0330

    Introducing to bootstrap and javaScript

---
+++ script.js
@@ -0,0 +1,116 @@
+// console.log("Hello world!");
+
+// () => {
+//   let firstName = "Ali";
+//   console.log(firstName);
+// };
+
+// //Using let keyword
+// let firstName = "Ali";
+// console.log(firstName);
+
+// //Using const keyword
+// const Pi = 3.14;
+// console.log(Pi);
+
+// /****************
+//  * Fuctions
+//  */
+
+// function sumFunction(input1, input2) {
+//   console.log(input1 + input2);
+// }
+
+// sumFunction(1, 2);
+
+// const minusFunction = (input1, input2) => {
+//   console.log(input1 - input2);
+// };
+
+// if function defined as const like line 26 you can't do below task.
+// minusFunction = 10;
+
+// minusFunction(1, 2);
+
+/***********************
+ * Operators.
+ */
+
+// Arithmetic Operators
+// let x = 10 + 5;
+// let y = 10 - 5;
+// let z = 10 * 5;
+// let i = 10 / 2;
+
+// let number = 10;
+// let stringDigits = "10";
+
+// // Logical Operators
+// let condition = number == stringDigits;
+// console.log(condition);
+
+// let notCondition = number != "10";
+// console.log(notCondition);
+
+// let otherCondition = number === stringDigits;
+// console.log(otherCondition);
+
+// let notOtherCondition = number !== "10";
+// console.log(notOtherCondition);
+
+// < > <= >= && ||
+
+/****************
+ * Data types
+ */
+// let number = 0;
+// console.log(number);
+
+// let emptyString = "";
+// let birthYear = "1995";
+// let birthMonth = `09`;
+// let birthDay = "Sunday";
+// console.log("string birth year:");
+// console.log(birthYear);
+
+// let condition = true;
+// console.log(condition);
+
+// let collection = [1, 2, 3];
+// console.log(collection);
+
+// let randomCollection = [1, "Eh", true];
+// console.log(randomCollection);
+
+// // How to cast data types to each other:
+// let stringNumber = number.toString();
+// let stringNumber2 = `${number}`;
+
+// let numericBirthYear = Number(birthYear);
+// console.log("numeric birth year");
+// console.log(numericBirthYear);
+
+// let numericBirthDay = Number(birthDay);
+// console.log(numericBirthDay);
+
+// if (Number.isNaN(numericBirthDay)) {
+//   console.log("BirthDay is not a number");
+// }
+
+// let numericCondition = Number(condition);
+// console.log("numeric condition");
+// console.log(numericCondition);
+
+// let booleanNumber = Boolean(number);
+// console.log("Boolean number");
+// console.log(booleanNumber);
+
+// let booleanBirthYear = Boolean(birthYear);
+// console.log("Boolean birth year");
+// console.log(booleanBirthYear);
+
+// let booleanEmptyString = Boolean(emptyString);
+// console.log("Boolean birth year");
+// console.log(booleanEmptyString);
+
+// console.log(`${booleanBirthYear}`);
